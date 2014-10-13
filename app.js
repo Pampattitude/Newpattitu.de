0d55ee0b21eafa3ef17e94325e2ef334ae0b1289
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-13 23:02:11 +0200

    / & /lib/: renamed console module to "printer", added constants for development, preproduction and production modes and clean code a bit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 'use strict';
 
+// Awake
+// Set environment mode; possible values: 'development', 'preproduction', 'production'
+var mode = process.NODE_ENV;
+if (!mode ||
+    !('development' == mode || 'preproduction' == mode || 'production' == mode)) {
+    process.NODE_ENV = 'development';
+    mode = process.NODE_ENV;
+}
+// !Awake
+
 var async               = require('async');
 var cluster             = require('cluster');
 var express             = require('express');
@@ -7,81 +17,92 @@ var helmet              = require('helmet');
 var mongoose            = require('mongoose');
 var requireDir          = require('require-dir');
 
-var console             = require('./lib/console');
 var constants           = require('./lib/constants');
+var printer             = require('./lib/printer');
 
-if (cluster.isMaster) {
-    global.processId = 'Master';
+var main = function() {
+    printer.info('Started application in ' + process.NODE_ENV + ' mode');
 
-    var clusterPerCpu   = ('production' == process.NODE_ENV ? 4 : 1);
-    var clusterCount    = parseInt(require('os').cpus().length * clusterPerCpu);
+    if (cluster.isMaster) {
+        global.processId = 'Master';
 
-    cluster.on('fork', function(worker) {
-        console.info('Worker #' + worker.id + ' created');
-    });
-    cluster.on('exit', function (worker) {
-        console.warn('Worker #' + worker.id + ' died, forking a new one');
-        cluster.fork();
-    });
+        var clusterPerCpu   = ('production' == process.NODE_ENV ? 4 : 1);
+        var clusterCount    = parseInt(require('os').cpus().length * clusterPerCpu);
 
-    for (var i = 0 ; i < clusterCount ; ++i)
-        cluster.fork();
+        cluster.on('fork', function(worker) {
+            printer.info('Worker #' + worker.id + ' created');
+        });
+        cluster.on('exit', function (worker) {
+            printer.warn('Worker #' + worker.id + ' died, forking a new one');
+            cluster.fork();
+        });
 
-    mongoose.connect(constants.databaseUri);
-    mongoose.connection.on('error', function (err) {
-        console.error('Could not open DB connection: ' + err);
-        mongoose.connection.close();
-        return process.exit(1);
-    });
+        for (var i = 0 ; i < clusterCount ; ++i)
+            cluster.fork();
 
-    return mongoose.connection.once('open', function () {
-        console.info('Connected to DB "' + constants.databaseUri + '"');
+        mongoose.connect(constants.databaseUri);
+        mongoose.connection.on('error', function (err) {
+            printer.error('Could not open DB connection: ' + err);
+            mongoose.connection.close();
+            return process.exit(1);
+        });
 
-        requireDir(__dirname + '/models/');
-        console.info('Models sync\'ed');
+        return mongoose.connection.once('open', function () {
+            printer.info('Connected to DB "' + constants.databaseUri + '"');
 
-	return async.series([
-            // Drop old sessions if needed
-            function(serieCallback) {
-                if (mongoose.connection.collections['sessions']) {
-                    return mongoose.connection.collections['sessions'].drop(function(err) {
-                        if (err) return serieCallback(err);
+            requireDir(__dirname + '/models/');
+            printer.info('Models sync\'ed');
 
-                        console.info('Sessions dropped');
-                        return serieCallback();
-                    });
+	    return async.series([
+                // Drop old sessions if needed
+                function(serieCallback) {
+                    if (mongoose.connection.collections.sessions) {
+                        return mongoose.connection.collections.sessions.drop(function(err) {
+                            if (err) return serieCallback(err);
+
+                            printer.info('Sessions dropped');
+                            return serieCallback();
+                        });
+                    }
+
+                    return serieCallback();
+                },
+                // Launch daemons
+                function(serieCallback) {
+                    // TODO (Pampa): implement daemons
+                    return serieCallback();
+                },
+	    ], function(err) {
+                if (err) {
+                    printer.error(err);
+                    return process.exit(1);
                 }
 
-                return serieCallback();
-            },
-            // Launch daemons
-            function(serieCallback) {
-                // TODO (Pampa): implement daemons
-                return serieCallback();
-            },
-	], function(err) {
-            if (err) {
-                console.error(err);
-                return process.exit(1);
-            }
-
-            return ; // Return nothing, app will stay alive because of open database connection
+                return ; // Return nothing, app will stay alive because of open database connection
+            });
         });
-    });
-}
-else {
-    global.processId = 'Worker #' + cluster.worker.id;
+    }
+    else {
+        global.processId = 'Worker #' + cluster.worker.id;
 
-    mongoose.connect(constants.databaseUri);
-    mongoose.connection.on('error', function (err) {
-        console.error('Could not open DB connection: ' + err);
-        mongoose.connection.close();
-        return process.exit(1);
-    });
+        mongoose.connect(constants.databaseUri);
+        mongoose.connection.on('error', function (err) {
+            printer.error('Could not open DB connection: ' + err);
+            mongoose.connection.close();
+            return process.exit(1);
+        });
 
-    return mongoose.connection.once('open', function () {
-        console.info('Connected to DB "' + constants.databaseUri + '"');
-        requireDir(__dirname + '/models/');
-        console.info('Models sync\'ed');
-    });
-}
+        return mongoose.connection.once('open', function () {
+            printer.info('Connected to DB "' + constants.databaseUri + '"');
+            requireDir(__dirname + '/models/');
+            printer.info('Models sync\'ed');
+
+            return runServer();
+        });
+    }
+};
+
+var runServer = function() {
+};
+
+return main();
