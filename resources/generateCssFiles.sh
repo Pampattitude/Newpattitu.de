ba7d327785e6724922f88504384458eb3a90ea1f
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-14 20:21:45 +0200

    /resources/: added script to generate .css files from .scss

---
+++ resources/generateCssFiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PWD="$(dirname $0)"
+DIRFILES="$(ls $PWD/scss/*.scss)"
+
+for FILE in $DIRFILES; do
+    NEWFILE="$PWD/css/$(basename $FILE .scss).css"
+    echo "Generating \"$(basename \"$NEWFILE\") from $(basename \"$FILE\")..."
+    scss "$FILE" "$NEWFILE"
+    echo "\"$(basename \"$NEWFILE\") generated"
+done
+
+echo "Cleaning .map files in \"$PWD/css/\" folder..."
+rm -vf "$PWD/css/"*.map
+echo ".map files in \"$PWD/css/\" folder cleaned"
