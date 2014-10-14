643a57ec15d09af47b3f0c0a2e8a88cb423289ce
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-14 21:58:21 +0200

    /resources/ & /tools/: added grid generator tool and generated grid CSS

---
+++ tools/gridGenerator.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var padding = {v:0, h:0}; // px
+
+var modeList = [
+    {
+        media: '@media all',
+        suffix: '',
+    },
+    {
+        media: '@media only screen and (max-width: 991px)',
+        suffix: '-handheld',
+    }
+];
+
+for (var modeIdx in modeList) {
+    var mode = modeList[modeIdx];
+
+    console.log(mode.media + ' {');
+
+    var gridSizeList = [2, 3, 4, 5, 6, 8, 10, 12, 16];
+
+    console.log('  [class*="grid-"] {');
+    console.log('    width: 100%;');
+    console.log('    margin: 0;');
+    console.log('  }');
+
+    for (var gridSizeIdx in gridSizeList) {
+        console.log();
+
+        var gridSize = gridSizeList[gridSizeIdx];
+
+        console.log('  .grid-' + gridSize + ':after {');
+        console.log('    content: "";');
+        console.log('    display: block;');
+        console.log('    height: 0;');
+        console.log('    clear: both;');
+        console.log('    visibility: hidden;');
+        console.log('  }');
+
+        console.log();
+
+        console.log('  .grid-' + gridSize + ' > [class*="cell' + mode.suffix + '"] {');
+        console.log('    width: 0%;');
+        console.log('    display: inline-block;');
+        console.log('    box-sizing: border-box;');
+        console.log('    float: left;');
+        console.log('    margin: 0 auto;');
+        console.log('    padding: ' + padding.v + 'px ' + padding.h + 'px;');
+        console.log('  }');
+
+        for (var n = 1 ; gridSize >= n ; ++n) {
+            console.log('  .grid-' + gridSize + ' > .cell' + mode.suffix + '-' + n + ' {');
+            console.log('    width: ' + ((parseInt(100 * n / gridSize * 100).toFixed(2)) / 100) + '%;');
+            console.log('  }');
+        }
+    }
+
+    console.log('}')
+}
