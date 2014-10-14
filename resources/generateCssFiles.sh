433cb31c1520f84e5a58774066064400b0f25e83
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-14 21:56:35 +0200

    /resources/: added mkdir -p of css directory in case it does not exist yet

--- resources/generateCssFiles.sh
+++ resources/generateCssFiles.sh
@@ -3,6 +3,8 @@
 PWD="$(dirname $0)"
 DIRFILES="$(ls $PWD/scss/*.scss)"
 
+mkdir -p "$PWD/css"
+
 for FILE in $DIRFILES; do
     NEWFILE="$PWD/css/$(basename $FILE .scss).css"
     echo "Generating \"$(basename \"$NEWFILE\") from $(basename \"$FILE\")..."
