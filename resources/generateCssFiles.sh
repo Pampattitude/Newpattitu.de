be309d6bb1b402471951e84d744cd8f8d79c67cf
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-16 17:35:29 +0200

    /resources/: generated CSS files are now minified as much as possible

--- resources/generateCssFiles.sh
+++ resources/generateCssFiles.sh
@@ -9,6 +9,6 @@ for FILE in $DIRFILES; do
     NEWFILE="$PWD/css/$(basename $FILE .scss).css"
     echo "Generating \"$(basename \"$NEWFILE\") from $(basename \"$FILE\")..."
     rm -f "$NEWFILE" 2>&1 > /dev/null
-    scss -C --sourcemap=none --style compact --stop-on-error "$FILE" "$NEWFILE"
+    scss -C --sourcemap=none --style compressed --stop-on-error "$FILE" "$NEWFILE"
     echo "\"$(basename \"$NEWFILE\") generated"
 done
