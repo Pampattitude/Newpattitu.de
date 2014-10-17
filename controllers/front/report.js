04f05a4bea7cace10816b9894d6c5f5eb958c154
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-17 18:37:43 +0200

    /controllers/front/: fixed page name for report page

--- controllers/front/report.js
+++ controllers/front/report.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.page = function(req, res, callback) {
-    res.locals.title = 'Home';
+    res.locals.title = 'Report';
 
     res.locals.page = 'pages/report.html';
     res.locals.hidePageMenu = true;
