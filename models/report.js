ec0f81b2cea54d786266faf5eb823fad25396994
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-17 17:44:32 +0200

    /models/: added status to Report model

--- models/report.js
+++ models/report.js
@@ -10,6 +10,8 @@ var schema = new mongoose.Schema({
     author: {type: String, unique: true},
     text: {type: String},
 
+    status: {type: String, enum: ['new', 'fixed', 'wontFix']},
+
     created: {type: Date, default: Date.now},
 });
 
