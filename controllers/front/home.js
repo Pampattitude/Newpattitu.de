fc2cefc8e509df09fc1a0e4898c62104754343b2
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-17 18:32:57 +0200

    /controllers/front/ & /views/common/: top buttons are now active when on a related page

--- controllers/front/home.js
+++ controllers/front/home.js
@@ -4,6 +4,7 @@ exports.page = function(req, res, callback) {
     res.locals.title = 'Home';
 
     res.locals.page = 'pages/home.html';
+    res.locals.activeTopMenu = 'home';
 
     return callback();
 };
