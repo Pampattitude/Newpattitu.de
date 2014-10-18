42e5d3f3d814b892df67b15a1efe9bc5a42625d8
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-18 09:16:52 +0200

    /resources/ & /views/: re-added top header, made navigation alrways fixed and updated Twitter page menu a bit

--- resources/js/front.js
+++ resources/js/front.js
@@ -2,20 +2,6 @@ $.getScript('/js/_textareaResize.js');
 
 (function() {
     $(document).ready(function() {
-        /* Fixed top menu */
-        var minHeight = $('#pmp-nav-desktop')[0].getBoundingClientRect().top +
-            $(this).scrollTop();
-        var nav = $('#pmp-nav-fixed');
-
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > minHeight) {
-                nav.removeClass('hidden-desktop');
-            } else {
-                nav.addClass   ('hidden-desktop');
-            }
-        });
-        /* !Fixed top menu */
-
         /* Konami code to access BO */
         new Konami('/back-office');
         /* !Konami code to access BO */
