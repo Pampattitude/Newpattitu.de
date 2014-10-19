75825fa1886c23e11ab0c177a66a4ce618a9e0ec
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-19 12:34:34 +0200

    /resources/js/: added script to add target=_blank and icon to external links

--- resources/js/front.js
+++ resources/js/front.js
@@ -2,6 +2,26 @@ $.getScript('/js/_textareaResize.js');
 
 (function() {
     $(document).ready(function() {
+        /* External links */
+        var host = window.location.host;
+
+        $('a:not(.no-parse)').each(function() {
+            var elem = this;
+
+            if ($(elem).hasClass('internal-link') || // Internal, already treated
+                $(elem).hasClass('external-link')) // External, already treated
+                return ;
+
+            if (/^\/[^\/]/.test($(elem).attr('href')) ||
+                (new RegExp('(http:)?\/\/' + host)).test($(elem).attr('href'))) { // Internal
+                $(elem).addClass('internal-link');
+                return ;
+            }
+
+            $(elem).addClass('external-link').attr('target', '_blank').append('&nbsp;&nbsp;<span class="icon icon-newtab"></span>');
+        });
+        /* !External links */
+
         /* Konami code to access BO */
         new Konami('/back-office');
         /* !Konami code to access BO */
