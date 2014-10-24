ce2297b5dd692562a5bedb798ed37a4dff38715c
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-24 18:50:25 +0200

    /controllers/front/: fixed bug where compressedCaption / compressedText would override caption / text

--- controllers/front/article.js
+++ controllers/front/article.js
@@ -14,17 +14,17 @@ exports.page = function(req, res, callback) {
         if (!article) return res.redirect('/404');
 
         res.locals.article = article;
-        res.locals.article.caption = res.locals.article.compressedCaption;
-        delete res.locals.article.compressedCaption;
-
-        res.locals.article.text = res.locals.article.compressedText;
-        delete res.locals.article.compressedText;
-
         res.locals.title = article.title;
 
         ++article.views;
         return article.save(function(err) {
             if (err) return callback(err);
+
+            res.locals.article.caption = res.locals.article.compressedCaption;
+            delete res.locals.article.compressedCaption;
+
+            res.locals.article.text = res.locals.article.compressedText;
+            delete res.locals.article.compressedText;
 
             return mongoose.model('Comment').find({article: article._id}).sort({created: 1}).populate('user').exec(function(err, comments) {
                 if (err) return callback(err);
@@ -44,23 +44,22 @@ exports.page = function(req, res, callback) {
 exports.previewPage = function(req, res, callback) {
     res.locals.page = 'pages/article.html';
     res.locals.activeTopMenu = 'blog';
+    res.locals.isPreview = true;
 
     return mongoose.model('Article').findOne({technicalName: req.params.articleTechnicalName}, function(err, article) {
         if (err) return callback(err);
         if (!article) return res.redirect('/back-office/404');
 
         res.locals.article = article;
-        res.locals.article.caption = res.locals.article.compressedCaption;
-        delete res.locals.article.compressedCaption;
-
-        res.locals.article.text = res.locals.article.compressedText;
-        delete res.locals.article.compressedText;
-
         res.locals.title = article.title + ' - Preview';
-
-        ++article.views;
         return article.save(function(err) {
             if (err) return callback(err);
+
+            res.locals.article.caption = res.locals.article.compressedCaption;
+            delete res.locals.article.compressedCaption;
+
+            res.locals.article.text = res.locals.article.compressedText;
+            delete res.locals.article.compressedText;
 
             // Finally, set user if user is set
             if (req.session.user)
