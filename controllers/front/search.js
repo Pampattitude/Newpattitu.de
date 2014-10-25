a2111120490aeb500b79c4c7f6a01e6feee43b81
Guillaume Delahodde <guillaume.delahodde@ezeeworld.com>
2014-10-25 18:47:00 +0200

    /controllers/front/: fixed bug with unshown caption in search page in front

--- controllers/front/search.js
+++ controllers/front/search.js
@@ -20,13 +20,6 @@ exports.page = function(req, res, callback) {
         if (err) return callback(err);
 
         res.locals.articleList = articles;
-        res.locals.articleList.forEach(function(elem) {
-            elem.caption = elem.compressedCaption;
-            delete elem.compressedCaption;
-
-            elem.text = elem.compressedText;
-            delete elem.compressedText;
-        });
 
         res.locals.searchString = req.query.search;
 
