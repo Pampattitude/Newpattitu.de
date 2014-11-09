68d6ad23a4840c61796f1d77aaa17b79ccabb24b
Guillaume Delahodde <guichan@hotmail.fr>
2014-11-09 11:21:02 +0100

    /controllers/middleware/: various fixes in front page view stats middleware

--- controllers/middleware/front.js
+++ controllers/middleware/front.js
@@ -5,14 +5,19 @@ var url = require('url');
 var stattitude = require('../../lib/stattitude');
 
 exports.postPageViewStat = function(req, res, next) {
-    if (/^\/ajax\//.test(req.url))
+    if (/^\/ajax\//.test(req.url || ''))
         return next(); // Skip /ajax/* URLs like Twitter get
 
+    var pageUrl = req.url || '/';
+
     var referrer = req.headers.referer || req.headers.Referer ||
-            req.headers.referrer || req.headers.Referrer;
+        req.headers.referrer || req.headers.Referrer;
+    if (referrer)
+        referrer = url.parse(referrer || '').host;
+
     stattitude.post('pageView', {
-        page: req.url,
-        referrer: url.parse(referrer).host
+        page: pageUrl,
+        referrer: referrer
     }); // Do not wait for reply
     return next();
 };
