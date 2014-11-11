59c2e040169c646886cdf2c44cd8e13bbb453daf
Guillaume Delahodde <guichan@hotmail.fr>
2014-11-11 15:09:57 +0100

    /controllers/front/ & /lib/: fixed the RSS feed, should be OK now

--- controllers/front/rss.js
+++ controllers/front/rss.js
@@ -12,27 +12,27 @@ exports.get = function(req, res, callback) {
     res.locals.page = 'pages/home.html';
     res.locals.activeTopMenu = 'blog';
 
-    return mongoose.model('Article').find().sort({created: -1}).limit(constants.rssMaxFeedItemCount).exec(function(err, articles) {
+    return mongoose.model('Article').find({activated: true}).sort({created: -1}).limit(constants.rssMaxFeedItemCount).exec(function(err, articles) {
         if (err)
             return callback(err);
 
         var rss = '';
         rss += '<?xml version="1.0"?>\n';
-        rss += '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">\n';
+        rss += '<rss version="2.0">\n';
         rss += '  <channel>\n';
-        rss += '    <atom:link href="' + req.protocol + '://' + req.get('host') + req.originalUrl + '" rel="self" type="application/rss+xml" />\n';
         rss += '    <title>Pampattitu.de</title>\n';
-        rss += '    <description>Guillaume "Pampa" Delahodde\'s very own website</description>\n';
-        rss += '    <link>' + constants.serverBaseUrl + '</link>\n';
+        rss += '    <link>' + constants.serverHostAccess + '</link>\n';
+        rss += '    <description>Guillaume "Pampa" Delahodde\'s blog</description>\n';
         rss += '    <language>en-us</language>\n';
 
         if (articles) {
             articles.forEach(function(elem) {
                 rss += '    <item>\n';
-                rss += '      <title>' + utils.stripHtml(elem.title) + '</title>\n';
-                rss += '      <link>' + constants.serverBaseUrl + '/article/' + elem.technicalName + '</link>\n';
-                rss += '      <description>' + utils.stripHtml(elem.compressedCaption) + '</description>\n';
-                rss += '      <guid>' + constants.serverBaseUrl + '/article/' + elem.technicalName + '</guid>\n';
+                rss += '      <title>' + utils.dtrim(utils.stripHtml(elem.title)) + '</title>\n';
+                if ('flash' != elem.type) // Do not add link if article with no link
+                    rss += '      <link>' + constants.serverHostAccess + '/article/' + elem.technicalName + '</link>\n';
+                rss += '      <description>' + utils.dtrim(utils.stripHtml(elem.compressedCaption)).replace(/&NewLine;/g, '') + '</description>\n';
+                rss += '      <guid isPermaLink="false">article/' + elem.technicalName + '</guid>\n';
                 rss += '      <pubDate>' + elem.created.toUTCString() + '</pubDate>\n';
                 rss += '    </item>\n';
             });
@@ -41,7 +41,7 @@ exports.get = function(req, res, callback) {
         rss += '  </channel>\n';
         rss += '</rss>\n'
 
-        res.set('Content-Type', 'text/xml');
+        res.set('Content-Type', 'application/rss+xml');
         return callback(null, rss);
     });
 };
