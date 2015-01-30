ec683b9309d5524a2c6f33620c6d28f9988d9567
Guillaume Delahodde <guichan@hotmail.fr>
2015-01-30 14:44:25 +0100

    /lib/ & /tools/daemons/: various bugfixes / cosmetic changes to mail daemon

--- tools/daemons/inactivityMail.js
+++ tools/daemons/inactivityMail.js
@@ -3,19 +3,15 @@
 var mongoose = require('mongoose');
 
 var minInactivityTime = 7; //days
-var lastTimeNotified = null;
 var minNotificationRetryTime = 1; //days
+var lastTimeNotified = new Date();
+lastTimeNotified.setDate(lastTimeNotified.getDate() - (minNotificationRetryTime + 1));
 
 module.exports = function(callback) {
-    if (!lastTimeNotified) {
-        lastTimeNotified = new Date();
-        lastTimeNotified.setDate(lastTimeNotified.getDate() - (minNotificationRetryTime + 1));
-    }
-
     var startInactivityTime = new Date();
     startInactivityTime.setDate(startInactivityTime.getDate() - minInactivityTime);
 
-    return mongoose.model('Article').find({lastUpdated: {$gte: startInactivityTime}}).sort({lastUpdated: -1}).limit(1).exec(function(err, articles) {
+    return mongoose.model('Article').find({lastUpdated: {$gte: startInactivityTime}}).exec(function(err, articles) {
         if (err) return callback(err);
         if (!articles.length) { // Maybe notify
             var nextNotificationMinTime = lastTimeNotified;
